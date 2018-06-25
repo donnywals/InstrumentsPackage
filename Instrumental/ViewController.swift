2fa46cfb5648fe63f08a89a07c6421545a375177
Donny Wals <donny.wals@bamtechmedia.com>
2018-06-25 10:43:41 +0200

    initial commit

--- Instrumental/ViewController.swift
+++ Instrumental/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  let tableView = UITableView()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    tableView.register(DemoCell.self, forCellReuseIdentifier: DemoCell.identifier)
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(tableView)
+    NSLayoutConstraint.activate([
+      tableView.topAnchor.constraint(equalTo: view.topAnchor),
+      tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+      tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+      tableView.leftAnchor.constraint(equalTo: view.leftAnchor)])
+
+    tableView.dataSource = self
+    tableView.delegate = self
+
+    [[1, 2], [2, 3]].enumerated().map { index, element in
+      print(index, element)
+    }
+  }
+}
+
+extension ViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1000
   }
 
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: DemoCell.identifier, for: indexPath)
+    cell.textLabel?.text = "Cell at \(indexPath)"
 
+    let id = OSSignpostID(log: logger, object: cell)
+    os_signpost(type: .begin, log: logger, name: "Parsing", signpostID: id, "Parsing SIZE:%@", "256")
+
+    return cell
+  }
+}
+
+import os.signpost
+let logger = OSLog(subsystem: "com.dw.json", category: "Networking")
+
+extension ViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    let id = OSSignpostID(log: logger, object: cell)
+    os_signpost(type: .end, log: logger, name: "Parsing", signpostID: id, "Parsing done")
+    os_signpost(type: .begin, log: logger, name: "Cell Display", signpostID: id, "Will display indexPath: %@", "\(indexPath.row)")
+  }
+
+  func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    let id = OSSignpostID(log: logger, object: cell)
+    os_signpost(type: .end, log: logger, name: "Cell Display", signpostID: id, "Did end displaying indexPath: %@", "\(indexPath)")
+  }
+}
+
+class DemoCell: UITableViewCell {
+  static let identifier = "DemoCell"
 }
 
